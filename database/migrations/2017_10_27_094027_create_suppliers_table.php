f3f287c36d0e64bc1c5f6ae4cf809e01b1cf4a2f
SU-RU <oooaa0727@gmail.com>
2017-11-17 15:24:35 +0800

    alter : supplier schema

--- database/migrations/2017_10_27_094027_create_suppliers_table.php
+++ database/migrations/2017_10_27_094027_create_suppliers_table.php
@@ -15,16 +15,17 @@ class CreateSuppliersTable extends Migration
     {
         Schema::create('suppliers', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('S_id')->unquie();
             $table->string('S_name');
-            $table->string('S_TaxID');
             $table->string('S_POC');
             $table->string('S_contact');
             $table->string('S_postal');
             $table->string('S_address');
-            $table->string('S_phone');
+            $table->string('S_phone')->nullable();
             $table->string('S_cellphone');
-            $table->string('S_fax');
-            $table->string('S_email');
+            $table->string('S_fax')->nullable();
+            $table->string('S_email')->nullable();
+            $table->string('S_TaxID')->nullable();
             $table->timestamps();
         });
     }
